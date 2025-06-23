34da086b1f2c9f1de8064531814107256eea0df3
nipun <nipuncod@gmail.com>
2025-06-23 20:48:37 +0530

    basic working script complete

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import csv
+from pathlib import Path
 
-import pandas as pd
+import pandas as pd  # type: ignore[import-untyped]
 
-# Converting the excel file to csv using pandas **(I should not do this and instead work with pandas)**
-excel_file = "input_files/my-word-list.xlsx"
-df = pd.read_excel(excel_file)  # noqa: PD901
-df.to_csv("output_files/my-word-list.csv", index=False)
+# Paths
+excel_path = Path("data/nipun-word-list.xlsx")
+csv_path = Path("data/nipun-word-list.csv")
+word_meaning_path = Path("data/word-meaning.csv")
+meaning_word_path = Path("data/meaning-word.csv")
 
+# Convert excel to csv
+pd.read_excel(excel_path).to_csv(csv_path, index=False)
+
+with csv_path.open(newline="", encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)  # Skip header
+    next(csv_reader)  # Skip metadata
+
+    # Load data into memory (since generators can be used only once)
+    rows = list(csv_reader)
+
+# Preprocess data (wrap part of speech in parentheses)
+for row in rows:
+    row[1] = f"({row[1]})"
+
+# Write word -> meaning
+with word_meaning_path.open("w", newline="", encoding="utf-8") as file:
+    csv_writer = csv.writer(file, delimiter="\t", lineterminator="\n")
+    for word, part, definition in rows:
+        csv_writer.writerow([f"{part} {word}", definition])
+
+# Write meaning -> word
+with meaning_word_path.open("w", newline="", encoding="utf-8") as file:
+    csv_writer = csv.writer(file, delimiter="\t", lineterminator="\n")
+    for word, part, definition in rows:
+        csv_writer.writerow([f"{part} {definition}", word])
